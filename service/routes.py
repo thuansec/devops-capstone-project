8f239b23b992d4eb21ddfbb5c69ae1cfd906f8bd
thuansec <thuannguyen.sec@gmail.com>
2025-02-26 10:31:36 -0500

    Reformatted service/routes.py

--- service/routes.py
+++ service/routes.py
@@ -96,7 +96,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
-@app.route("/accounts/<int:account_id>", methods=["PUT"]
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
     Update an Account
